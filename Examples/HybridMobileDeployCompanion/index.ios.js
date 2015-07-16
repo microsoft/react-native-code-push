d07b96a47caa65d6af12414be632fb4a2c215c3a
Will Anderson <wiand@microsoft.com>
2015-07-16 15:31:34 -0700

    Update example to use new HybridMobileDeploy implementation

--- Examples/HybridMobileDeployCompanion/index.ios.js
+++ Examples/HybridMobileDeployCompanion/index.ios.js
@@ -9,13 +9,41 @@ var {
   AppRegistry,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } = React;
 
-var HybridMobileDeploy = require('react-native-hybrid-mobile-deploy')('http://localhost:3000/', 'HybridMobileDeployCompanion');
+var Button = require("react-native-button");
+
+var HybridMobileDeploy = require('react-native-hybrid-mobile-deploy')('http://localhost:3000/', '<deployment key here>');
 
 var HybridMobileDeployCompanion = React.createClass({
+  componentDidMount: function() {
+    this.fetchData();
+  },
+  fetchData: function() {
+      HybridMobileDeploy.queryUpdate((err, update) => {
+        this.setState({ update: update, updateString: JSON.stringify(update) });
+      });
+  },
+  getInitialState: function() {
+    return { update: false, updateString: "" };
+  },
+  handlePress: function() {
+    console.log("pressed");
+  },
   render: function() {
+    var updateView;
+    if (this.state.update) {
+      updateView = (
+        <View>
+          <Text>Update Available: {'\n'} {this.state.update.scriptVersion} - {this.state.update.description}</Text>
+          <Button style={{color: 'green'}} onPress={this._handlePress}>
+            Update
+          </Button>
+        </View>
+      );
+    };
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -28,6 +56,7 @@ var HybridMobileDeployCompanion = React.createClass({
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
+        {updateView}
       </View>
     );
   }
