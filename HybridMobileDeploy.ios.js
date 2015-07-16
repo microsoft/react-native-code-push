45048f57dd66b1957e17740619f525ec8464837a
Will Anderson <wiand@microsoft.com>
2015-07-16 15:30:59 -0700

    Update HybridMobileDeploy js api with basic update query

--- HybridMobileDeploy.ios.js
+++ HybridMobileDeploy.ios.js
@@ -6,61 +6,28 @@
 'use strict';
 
 var NativeHybridMobileDeploy = require('NativeModules').HybridMobileDeploy;
+var requestFetchAdapter = require("./request-fetch-adapter.js");
 var semver = require('semver');
+var Sdk = require("hybrid-mobile-deploy-sdk/script/acquisition-sdk");
 var serverUrl;
 var appName;
+var sdk;
 
 var HybridMobileDeploy = {
-  checkForUpdate: function(version, callback) {
-    var url = serverUrl + 'latest/' + appName;
-    fetch(url)
-      .then(response => response.json())
-      .done(latest => {
-        if (semver.gt(latest.version, version)) {
-          callback(undefined, latest);
-        } else {
-          callback(undefined, false);
-        }
-      }, err => {
-        callback(err);
-      });
-  },
-  pollForUpdate: function(version, intervalDelay, callback) {
-    var interval;
-    var checkUpdate = () => {
-      this.checkForUpdate(version, (err, update) => {
-        if (err) {
-          callback(err);
-        } else if (update) {
-          interval && clearInterval(interval);
-          callback(undefined, update);
-        }
-      });
-    };
-    interval = setInterval(checkUpdate, intervalDelay);
-    checkUpdate();
-  },
-  getAvailableUpdates: function(callback) {
-    var url = serverUrl + 'updates/' + appName;
-    console.log(url);
-    fetch(url)
-      .then((response) => {
-        console.log(response);
-        return response.json()
-      })
-      .then((value) => {
-        console.log(value);
-        return value;
-      })
-      .then(callback);
+  queryUpdate: function(cb) {
+    var pkg = {nativeVersion: "1.2.3", scriptVersion: "1.2.0"};
+    sdk.queryUpdateWithCurrentPackage(pkg, cb);
   },
   installUpdate: function(update) {
     NativeHybridMobileDeploy.installUpdateFromUrl(update.updateUrl, update.bundleName, (err) => console.log(err), () => console.log("success"));
   }
 };
 
-module.exports = function(server, app) {
-  serverUrl = server;
-  appName = app;
+module.exports = function(serverUrl, deploymentKey, ignoreNativeVersion) {
+  sdk = new Sdk(requestFetchAdapter, {
+      serverUrl: serverUrl,
+      deploymentKey: deploymentKey,
+      ignoreNativeVersion: ignoreNativeVersion
+  });
   return HybridMobileDeploy; 
 };
