5a31a310844b7d16950c603c98f57410d69f6c1f
Geoffrey Goh <kg422@cornell.edu>
2015-12-04 14:45:02 -0800

    delete bundle file if it already exists

--- CodePushPackage.m
+++ CodePushPackage.m
@@ -302,6 +302,14 @@ NSString * const UnzippedFolderName = @"unzipped";
                     return;
                 }
             } else {
+                if ([[NSFileManager defaultManager] fileExistsAtPath:bundleFilePath]) {
+                    [[NSFileManager defaultManager] removeItemAtPath:bundleFilePath error:&error];
+                    if (error) {
+                        failCallback(error);
+                        return;
+                    }
+                }
+                
                 [[NSFileManager defaultManager] moveItemAtPath:downloadFilePath
                                                         toPath:bundleFilePath
                                                          error:&error];
