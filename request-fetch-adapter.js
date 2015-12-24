e6115deec9cf37140d0335082ea6db8bf8c73371
Geoffrey Goh <kg422@cornell.edu>
2015-12-23 21:55:25 -0800

    request-fetch-adapter

--- request-fetch-adapter.js
+++ request-fetch-adapter.js
@@ -1,30 +1,30 @@
-module.exports.request = function request(verb, url, body, callback) {
+let request = async (verb, url, body, callback) => {
     if (typeof body === "function") {
         callback = body;
         body = null;
     }
 
     var headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        "Accept": "application/json",
+        "Content-Type": "application/json"
     };
 
     if (body && typeof body === "object") {
         body = JSON.stringify(body);
     }
 
-    var statusCode;
-
-    fetch(url, {
-        method: verb,
-        headers: headers,
-        body: body
-    }).then(function(response) {
-        statusCode = response.status;
-        return response.text();
-    }).then(function(body) {
+    try {      
+        var statusCode;
+        let response = await fetch(url, {
+            method: verb,
+            headers: headers,
+            body: body
+        });
+        
+        let statusCode = response.status;
+        let body = await response.text();
         callback(null, {statusCode: statusCode, body: body});
-    }).catch(function(err) {
+    } catch (err) {
         callback(err);
-    });
+    }
 }
