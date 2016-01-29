73b744c60ed4f9035ab0ef018df5ee2b5abc6d0b
Geoffrey Goh <kg422@cornell.edu>
2016-01-28 20:23:10 -0800

    edit invalid package exception message

--- android/app/src/main/java/com/microsoft/codepush/react/CodePushInvalidPackageException.java
+++ android/app/src/main/java/com/microsoft/codepush/react/CodePushInvalidPackageException.java
@@ -2,6 +2,6 @@ package com.microsoft.codepush.react;
 
 public class CodePushInvalidPackageException  extends RuntimeException {
     public CodePushInvalidPackageException() {
-        super("Update is invalid - no files with extension .jsbundle or .bundle were found in the update package.");
+        super("Update is invalid - no files with extension .bundle, .js or .jsbundle were found in the update package.");
     }
 }
