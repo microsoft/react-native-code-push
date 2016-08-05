81e0f13ff9cab3fe0eff9417b743e1b6e1e17909
Mads Lee Jensen <madsleejensen@gmail.com>
2016-08-05 15:20:50 +0200

    bind 'this' to the react component in the callbacks codePushStatusDidChange(...) and codePushDownloadDidProgress(...)

--- CodePush.js
+++ CodePush.js
@@ -428,8 +428,8 @@ function codePushify(options = {}) {
           CodePush.notifyAppReady();
         } else {
           let rootComponentInstance = this.refs.rootComponent;
-          let syncStatusCallback = rootComponentInstance && rootComponentInstance.codePushStatusDidChange;
-          let downloadProgressCallback = rootComponentInstance && rootComponentInstance.codePushDownloadDidProgress;
+          let syncStatusCallback = rootComponentInstance && rootComponentInstance.codePushStatusDidChange.bind(rootComponentInstance);
+          let downloadProgressCallback = rootComponentInstance && rootComponentInstance.codePushDownloadDidProgress.bind(rootComponentInstance);
           CodePush.sync(options, syncStatusCallback, downloadProgressCallback);
           if (options.checkFrequency === CodePush.CheckFrequency.ON_APP_RESUME) {
             ReactNative.AppState.addEventListener("change", (newState) => {
