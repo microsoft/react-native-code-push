146355b5e0b3c49944fcf865943c279a72c908a5
Rahul Jiresal <rahul@aerofs.com>
2016-08-17 15:35:11 -0700

    Fix the hash mismatch happening due to .DS_Store file when running on simulator

--- ios/CodePush/CodePushUpdateUtils.m
+++ ios/CodePush/CodePushUpdateUtils.m
@@ -20,6 +20,14 @@ NSString * const ManifestFolderPrefix = @"CodePush";
     }
     
     for (NSString *fileName in folderFiles) {
+#if TARGET_IPHONE_SIMULATOR
+        // If the user is running this in a simulator, there is a chance
+        // that the .DS_Store file will mess up the calculated hash
+        // We must skip this file.
+        if ([fileName isEqualToString:@".DS_Store"]) {
+            continue;
+        }
+#endif
         NSString *fullFilePath = [folderPath stringByAppendingPathComponent:fileName];
         NSString *relativePath = [pathPrefix stringByAppendingPathComponent:fileName];
         BOOL isDir = NO;
