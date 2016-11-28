0a48e7cafabfc166e4668e765432a7e6ec0ff968
BO KANG <buptkang@gmail.com>
2016-11-28 13:41:05 -0800

    upgrade to RN 0.38

--- Examples/CodePushDemoApp/demo.js
+++ Examples/CodePushDemoApp/demo.js
@@ -103,7 +103,7 @@ class CodePushDemoApp extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to CodePush!
+          Welcome to CodePush, test.
         </Text>
         <TouchableOpacity onPress={this.sync.bind(this)}>
           <Text style={styles.syncButton}>Press for background sync</Text>
