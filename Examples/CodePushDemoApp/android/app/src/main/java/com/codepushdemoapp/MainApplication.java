347bbaf57d7bbad4f99445be603830ca35adc43a
BO KANG <buptkang@gmail.com>
2016-11-28 13:45:49 -0800

    upgrade to RN 0.38

--- Examples/CodePushDemoApp/android/app/src/main/java/com/codepushdemoapp/MainApplication.java
+++ Examples/CodePushDemoApp/android/app/src/main/java/com/codepushdemoapp/MainApplication.java
@@ -30,7 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new CodePush("ESrO1DeACI1wteWNKbjECBZqcRZvNkg_iCpgf", MainApplication.this, BuildConfig.DEBUG)      );
+          new CodePush("deployment-key-here", MainApplication.this, BuildConfig.DEBUG) );
     }
   };
 
