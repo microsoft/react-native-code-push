76b4f4d5582d8ff8dc441ceed85a9ee31d48196c
BO KANG <buptkang@gmail.com>
2016-11-28 13:58:13 -0800

    Upgrade to rn 0.38 (#624)
    * upgrade to RN 0.38

--- Examples/CodePushDemoApp/android/app/src/main/java/com/codepushdemoapp/MainApplication.java
+++ Examples/CodePushDemoApp/android/app/src/main/java/com/codepushdemoapp/MainApplication.java
@@ -8,9 +8,9 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import com.microsoft.codepush.react.CodePush;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,8 +20,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected String getJSBundleFile() {
       return CodePush.getJSBundleFile();
-    }
-
+    }    
     @Override
     protected boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -31,13 +30,18 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new CodePush("deployment-key-here", MainApplication.this, BuildConfig.DEBUG)
-      );
+          new CodePush("deployment-key-here", MainApplication.this, BuildConfig.DEBUG) );
     }
   };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
+    return mReactNativeHost;
+  }
+  
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
