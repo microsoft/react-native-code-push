25851d24a1edb9a41574f6487a201bee15946407
Christopher Dro <hello@blick-labs.com>
2017-01-23 18:21:03 -0800

    Update header imports for RN 0.40 (#670)
    This PR has been tested with the other two PRs for the RN 0.40.

--- ios/CodePush/CodePush.m
+++ ios/CodePush/CodePush.m
@@ -1,9 +1,9 @@
-#import "RCTAssert.h"
-#import "RCTBridgeModule.h"
-#import "RCTConvert.h"
-#import "RCTEventDispatcher.h"
-#import "RCTRootView.h"
-#import "RCTUtils.h"
+#import <React/RCTAssert.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTConvert.h>
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTRootView.h>
+#import <React/RCTUtils.h>
 
 #import "CodePush.h"
 
