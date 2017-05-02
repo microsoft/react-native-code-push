ddc3e1d9f754348b5407a6e00fcb0b732421bb1d
Sergey Akhalkov <sergey.akhalkov@akvelon.com>
2017-05-02 15:26:09 +0300

    Add ON_NEXT_SUSPEND install mode to the react-native-code-push.d.ts file

--- typings/react-native-code-push.d.ts
+++ typings/react-native-code-push.d.ts
@@ -304,7 +304,14 @@ declare namespace CodePush {
          * Indicates that you want to install the update, but don't want to restart the
          * app until the next time the end user resumes it from the background.
          */
-        ON_NEXT_RESUME
+        ON_NEXT_RESUME,
+
+        /**
+         * Indicates that you want to install the update when the app is in the background,
+         * but only after it has been in the background for "minimumBackgroundDuration" seconds (0 by default),
+         * so that user context isn't lost unless the app suspension is long enough to not matter.
+         */
+        ON_NEXT_SUSPEND
     }
 
     /**
