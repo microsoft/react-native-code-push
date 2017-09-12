d37ee85620d09893eea52af54dbf671beb807904
Andrew Jack <me@andrewjack.uk>
2017-09-12 08:56:12 +0100

    check if package metadata is null (#992)

--- android/app/src/main/java/com/microsoft/codepush/react/CodePush.java
+++ android/app/src/main/java/com/microsoft/codepush/react/CodePush.java
@@ -184,7 +184,7 @@ public class CodePush implements ReactPackage {
         JSONObject pendingUpdate = mSettingsManager.getPendingUpdate();
         if (pendingUpdate != null) {
             JSONObject packageMetadata = this.mUpdateManager.getCurrentPackage();
-            if (!isPackageBundleLatest(packageMetadata) && hasBinaryVersionChanged(packageMetadata)) {
+            if (packageMetadata == null || !isPackageBundleLatest(packageMetadata) && hasBinaryVersionChanged(packageMetadata)) {
                 CodePushUtils.log("Skipping initializeUpdateAfterRestart(), binary version is newer");
                 return;
             }
