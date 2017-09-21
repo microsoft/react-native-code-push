e786604dbeb953dcbdea26e13e9da7b1bcc68930
ruslan-bikkinin <v-rubikk@microsoft.com>
2017-09-21 15:16:02 +0300

    `Prevent Rollback crash when binary was updated.` transfer from master

--- android/app/src/main/java/com/microsoft/codepush/react/CodePushCore.java
+++ android/app/src/main/java/com/microsoft/codepush/react/CodePushCore.java
@@ -285,44 +285,30 @@ public class CodePushCore {
     public String getJSBundleFileInternal(String assetsBundleFileName) {
         this.mAssetsBundleFileName = assetsBundleFileName;
         String binaryJsBundleUrl = CodePushConstants.ASSETS_BUNDLE_PREFIX + assetsBundleFileName;
-        long binaryResourcesModifiedTime = this.getBinaryResourcesModifiedTime();
 
-        try {
-            String packageFilePath = mUpdateManager.getCurrentPackageBundlePath(this.mAssetsBundleFileName);
-            if (packageFilePath == null) {
-                // There has not been any downloaded updates.
-                CodePushUtils.logBundleUrl(binaryJsBundleUrl);
-                sIsRunningBinaryVersion = true;
-                return binaryJsBundleUrl;
+        String packageFilePath = mUpdateManager.getCurrentPackageBundlePath(this.mAssetsBundleFileName);
+        if (packageFilePath == null) {
+            // There has not been any downloaded updates.
+            CodePushUtils.logBundleUrl(binaryJsBundleUrl);
+            sIsRunningBinaryVersion = true;
+            return binaryJsBundleUrl;
+        }
+
+        JSONObject packageMetadata = this.mUpdateManager.getCurrentPackage();
+        if (isPackageBundleLatest(packageMetadata)) {
+            CodePushUtils.logBundleUrl(packageFilePath);
+            sIsRunningBinaryVersion = false;
+            return packageFilePath;
+        } else {
+            // The binary version is newer.
+            this.mDidUpdate = false;
+            if (!this.mIsDebugMode || hasBinaryVersionChanged(packageMetadata)) {
+                this.clearUpdates();
             }
 
-            JSONObject packageMetadata = this.mUpdateManager.getCurrentPackage();
-            Long binaryModifiedDateDuringPackageInstall = null;
-            String binaryModifiedDateDuringPackageInstallString = packageMetadata.optString(CodePushConstants.BINARY_MODIFIED_TIME_KEY, null);
-            if (binaryModifiedDateDuringPackageInstallString != null) {
-                binaryModifiedDateDuringPackageInstall = Long.parseLong(binaryModifiedDateDuringPackageInstallString);
-            }
-
-            String packageAppVersion = packageMetadata.optString("appVersion", null);
-            if (binaryModifiedDateDuringPackageInstall != null &&
-                    binaryModifiedDateDuringPackageInstall == binaryResourcesModifiedTime &&
-                    (isUsingTestConfiguration() || sAppVersion.equals(packageAppVersion))) {
-                CodePushUtils.logBundleUrl(packageFilePath);
-                sIsRunningBinaryVersion = false;
-                return packageFilePath;
-            } else {
-                // The binary version is newer.
-                this.mDidUpdate = false;
-                if (!this.mIsDebugMode || !sAppVersion.equals(packageAppVersion)) {
-                    this.clearUpdates();
-                }
-
-                CodePushUtils.logBundleUrl(binaryJsBundleUrl);
-                sIsRunningBinaryVersion = true;
-                return binaryJsBundleUrl;
-            }
-        } catch (NumberFormatException e) {
-            throw new CodePushUnknownException("Error in reading binary modified date from package metadata", e);
+            CodePushUtils.logBundleUrl(binaryJsBundleUrl);
+            sIsRunningBinaryVersion = true;
+            return binaryJsBundleUrl;
         }
     }
 
@@ -337,6 +323,12 @@ public class CodePushCore {
 
         JSONObject pendingUpdate = mSettingsManager.getPendingUpdate();
         if (pendingUpdate != null) {
+            JSONObject packageMetadata = this.mUpdateManager.getCurrentPackage();
+            if (!isPackageBundleLatest(packageMetadata) && hasBinaryVersionChanged(packageMetadata)) {
+                CodePushUtils.log("Skipping initializeUpdateAfterRestart(), binary version is newer");
+                return;
+            }
+
             try {
                 boolean updateIsLoading = pendingUpdate.getBoolean(CodePushConstants.PENDING_UPDATE_IS_LOADING_KEY);
                 if (updateIsLoading) {
@@ -372,6 +364,28 @@ public class CodePushCore {
 
     boolean isRunningBinaryVersion() {
         return sIsRunningBinaryVersion;
+    }
+
+    private boolean isPackageBundleLatest(JSONObject packageMetadata) {
+        try {
+            Long binaryModifiedDateDuringPackageInstall = null;
+            String binaryModifiedDateDuringPackageInstallString = packageMetadata.optString(CodePushConstants.BINARY_MODIFIED_TIME_KEY, null);
+            if (binaryModifiedDateDuringPackageInstallString != null) {
+                binaryModifiedDateDuringPackageInstall = Long.parseLong(binaryModifiedDateDuringPackageInstallString);
+            }
+            String packageAppVersion = packageMetadata.optString("appVersion", null);
+            long binaryResourcesModifiedTime = this.getBinaryResourcesModifiedTime();
+            return binaryModifiedDateDuringPackageInstall != null &&
+                    binaryModifiedDateDuringPackageInstall == binaryResourcesModifiedTime &&
+                    (isUsingTestConfiguration() || sAppVersion.equals(packageAppVersion));
+        } catch (NumberFormatException e) {
+            throw new CodePushUnknownException("Error in reading binary modified date from package metadata", e);
+        }
+    }
+
+    private boolean hasBinaryVersionChanged(JSONObject packageMetadata) {
+        String packageAppVersion = packageMetadata.optString("appVersion", null);
+        return !sAppVersion.equals(packageAppVersion);
     }
 
     boolean needToReportRollback() {
