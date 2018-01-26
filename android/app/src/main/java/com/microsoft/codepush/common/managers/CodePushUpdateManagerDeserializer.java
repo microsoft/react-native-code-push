cefe6361fe1ef588ea74dd856064a4862018941f
annakocheshkova <v-annkoc@microsoft.com>
2018-01-26 16:11:10 +0300

    Added update manager and classes connected to it.

---
+++ android/app/src/main/java/com/microsoft/codepush/common/managers/CodePushUpdateManagerDeserializer.java
@@ -0,0 +1,67 @@
+package com.microsoft.codepush.common.managers;
+
+import com.microsoft.codepush.common.datacontracts.CodePushLocalPackage;
+import com.microsoft.codepush.common.exceptions.CodePushGetPackageException;
+import com.microsoft.codepush.common.utils.CodePushUtils;
+
+import org.json.JSONObject;
+
+/**
+ * Wrapper around {@link CodePushUpdateManager}, returning data mapped to real objects.
+ */
+public class CodePushUpdateManagerDeserializer {
+
+    /**
+     * Instance of {@link CodePushUpdateManager}.
+     */
+    private CodePushUpdateManager mUpdateManager;
+
+    /**
+     * Creates new instance of deserializer using {@link CodePushUpdateManager}.
+     *
+     * @param updateManager instance of {@link CodePushUpdateManager}.
+     */
+    public CodePushUpdateManagerDeserializer(CodePushUpdateManager updateManager) {
+        mUpdateManager = updateManager;
+    }
+
+    /**
+     * Gets current package contents.
+     *
+     * @return object with current package info.
+     */
+    public CodePushLocalPackage getCurrentPackage() throws CodePushGetPackageException {
+        JSONObject currentPackage = mUpdateManager.getCurrentPackage();
+        if (currentPackage != null) {
+            return CodePushUtils.convertJsonObjectToObject(currentPackage, CodePushLocalPackage.class);
+        }
+        return null;
+    }
+
+    /**
+     * Gets package object using package identifier (hash).
+     *
+     * @param packageHash package identifier (hash).
+     * @return object with current package info.
+     */
+    public CodePushLocalPackage getPackage(String packageHash) throws CodePushGetPackageException {
+        JSONObject localPackage = mUpdateManager.getPackage(packageHash);
+        if (localPackage != null) {
+            return CodePushUtils.convertJsonObjectToObject(localPackage, CodePushLocalPackage.class);
+        }
+        return null;
+    }
+
+    /**
+     * Gets previous installed package object.
+     *
+     * @return previous installed package object.
+     */
+    public CodePushLocalPackage getPreviousPackage() throws CodePushGetPackageException {
+        JSONObject previousPackage = mUpdateManager.getPreviousPackage();
+        if (previousPackage != null) {
+            return CodePushUtils.convertJsonObjectToObject(previousPackage, CodePushLocalPackage.class);
+        }
+        return null;
+    }
+}
