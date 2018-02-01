893b07addc92b2c2435a94d104aeea347ba88e79
annakocheshkova <v-annkoc@microsoft.com>
2018-02-01 18:15:51 +0300

    Made TestPlatformUtils a sigleton.

--- android/app/src/androidTest/java/com/microsoft/codepush/common/utils/TestPlatformUtils.java
+++ android/app/src/androidTest/java/com/microsoft/codepush/common/utils/TestPlatformUtils.java
@@ -10,6 +10,36 @@ import java.io.File;
  * Platform specific implementation of utils (only for testing).
  */
 public class TestPlatformUtils implements PlatformUtils {
+
+    /**
+     * Instance of the utils implementation (singleton).
+     */
+    private TestPlatformUtils INSTANCE;
+
+    /**
+     * Private constructor to prevent creating utils manually.
+     */
+    private TestPlatformUtils() {
+    }
+
+    /**
+     * Provides instance of the utils class.
+     *
+     * @return instance.
+     */
+    public TestPlatformUtils getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new TestPlatformUtils();
+        }
+        return INSTANCE;
+    }
+
+    /**
+     * Returns folder path of update package (cordova-specific for testing).
+     *
+     * @param hash hash of the desired update.
+     * @return test folder path of update package.
+     */
     @Override public String getUpdateFolderPath(String hash) {
         return new File(new File(new File(Environment.getExternalStorageDirectory(), CodePushConstants.CODE_PUSH_FOLDER_PREFIX), hash), "www").getPath();
     }
