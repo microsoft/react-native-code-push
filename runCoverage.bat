7ed3a2090efa60acbb0cd3729b8371c71ec3639d
ruslan-bikkinin <v-rubikk@microsoft.com>
2018-02-02 16:00:26 +0300

    Fix scripts

--- runCoverage.bat
+++ runCoverage.bat
@@ -5,12 +5,13 @@ call npm i
 )
 
 rmdir /S /Q node_modules\react-native-code-push\android
-xcopy  /I /Q /E /S ..\android node_modules\react-native-code-push\android
+xcopy  /I /Q /E /S ..\android node_modules\react-native-code-push\android /Exclude:%cd%\..\coverageCopyIgnore.txt
 
 cd android
+call gradlew :react-native-code-push:clean
 call gradlew :react-native-code-push:build
 call gradlew :react-native-code-push:coverageReport
 
 cd ..\..
 
-echo "Coverage report results: file://%cd%/node_modules/react-native-code-push/android/app/build/reports/jacoco/coverageReport/html/com.microsoft.codepush.common/index.html"
+echo "Coverage report results: file://%cd%/coverageTestApp/node_modules/react-native-code-push/android/app/build/reports/jacoco/coverageReport/html/index.html"
--- runCoverage.sh
+++ runCoverage.sh
@@ -1,12 +1,13 @@
 #/bin/bash
 
 cd coverageTestApp
-if [ ! -d "coverageTestApp/node_modules" ]; then
+if [ ! -d "node_modules" ]; then
     npm i
 fi;
 
 rm -rf node_modules/react-native-code-push/android
 cp -R ../android node_modules/react-native-code-push/android
+rsync -r --exclude-from="../coverageCopyIgnore.txt" ../android node_modules/react-native-code-push/android 
 
 cd android
 ./gradlew :react-native-code-push:build
@@ -14,4 +15,4 @@ cd android
 
 cd ..
 
-echo "Coverage report results: file://$PWD/node_modules/react-native-code-push/android/app/build/reports/jacoco/coverageReport/html/com.microsoft.codepush.common/index.html"
+echo "Coverage report results: file://$PWD/node_modules/react-native-code-push/android/app/build/reports/jacoco/coverageReport/html/index.html"
