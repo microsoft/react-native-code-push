4da46283e79a6e3296c0de7541070c29fdb84055
annakocheshkova <v-annkoc@microsoft.com>
2018-02-09 12:37:20 +0300

    Refactored several more RN classes.

--- android/app/src/main/java/com/microsoft/codepush/common/utils/CodePushLogUtils.java
+++ android/app/src/main/java/com/microsoft/codepush/common/utils/CodePushLogUtils.java
@@ -17,8 +17,9 @@ public class CodePushLogUtils {
      * Represents wrapper on {@link Crashes#trackException(Throwable)} method.
      *
      * @param throwable exception instance.
+     * @param shouldLog <code>true</code> if log exception on device.
      */
-    public static void trackException(Throwable throwable) {
+    public static void trackException(Throwable throwable, boolean shouldLog) {
         try {
             Method method = Crashes.class.getMethod("trackException", Throwable.class);
             method.invoke(throwable);
@@ -32,8 +33,9 @@ public class CodePushLogUtils {
      *
      * @param throwable  exception instance.
      * @param properties additional properties.
+     * @param shouldLog <code>true</code> if log exception on device.
      */
-    public static void trackException(@NonNull Throwable throwable, Map<String, String> properties) {
+    public static void trackException(@NonNull Throwable throwable, Map<String, String> properties, boolean shouldLog) {
         try {
             Method method = Crashes.class.getMethod("trackException", Throwable.class, Map.class);
             method.invoke(throwable, properties);
--- android/app/src/main/java/com/microsoft/codepush/react/CodePushDialog.java
+++ android/app/src/main/java/com/microsoft/codepush/react/CodePushDialog.java
@@ -9,22 +9,39 @@ import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.microsoft.codepush.react.exceptions.CodePushUnknownException;
-import com.microsoft.codepush.react.utils.CodePushRNUtils;
 
-public class CodePushDialog extends ReactContextBaseJavaModule{
+/**
+ * Represents a react native dialog.
+ */
+public class CodePushDialog extends ReactContextBaseJavaModule {
 
+    /**
+     * Creates an instance of the {@link CodePushDialog}.
+     *
+     * @param reactContext instance of the {@link ReactApplicationContext}.
+     */
     public CodePushDialog(ReactApplicationContext reactContext) {
         super(reactContext);
     }
 
+    /**
+     * Displays dialog with the provided options.
+     *
+     * @param title           title of the dialog.
+     * @param message         message to be displayed.
+     * @param button1Text     text on the left button.
+     * @param button2Text     test on the right button.
+     * @param successCallback callback to handle "OK" events.
+     * @param errorCallback   callback to handle "Discard" events.
+     */
     @ReactMethod
     public void showDialog(final String title, final String message, final String button1Text,
                            final String button2Text, final Callback successCallback, Callback errorCallback) {
         Activity currentActivity = getCurrentActivity();
         if (currentActivity == null) {
-            // If getCurrentActivity is null, it could be because the app is backgrounded,
-            // so we show the dialog when the app resumes)
+
+            /* If getCurrentActivity is null, it could be because the app is backgrounded,
+            * so we show the dialog when the app resumes). */
             getReactApplicationContext().addLifecycleEventListener(new LifecycleEventListener() {
                 @Override
                 public void onHostResume() {
@@ -50,12 +67,20 @@ public class CodePushDialog extends ReactContextBaseJavaModule{
         }
     }
 
+    /**
+     * Internal method for actually showing the dialog.
+     *
+     * @param title           title of the dialog.
+     * @param message         message to be displayed.
+     * @param button1Text     text on the left button.
+     * @param button2Text     test on the right button.
+     * @param successCallback callback to handle "OK" events.
+     * @param currentActivity application activity.
+     */
     private void showDialogInternal(String title, String message, String button1Text,
                                     String button2Text, final Callback successCallback, Activity currentActivity) {
         AlertDialog.Builder builder = new AlertDialog.Builder(currentActivity);
-
         builder.setCancelable(false);
-
         DialogInterface.OnClickListener clickListener = new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -68,32 +93,27 @@ public class CodePushDialog extends ReactContextBaseJavaModule{
                         successCallback.invoke(1);
                         break;
                     default:
-                        throw new CodePushUnknownException("Unknown button ID pressed.");
+                        //TODO: track the exception here cause can't be thrown. "Unknown button ID pressed."
                 }
             }
         };
-
         if (title != null) {
             builder.setTitle(title);
         }
-
         if (message != null) {
             builder.setMessage(message);
         }
-
         if (button1Text != null) {
             builder.setPositiveButton(button1Text, clickListener);
         }
-
         if (button2Text != null) {
             builder.setNegativeButton(button2Text, clickListener);
         }
-
         try {
             AlertDialog dialog = builder.create();
             dialog.show();
         } catch (Exception e) {
-            CodePushRNUtils.log(e.toString());
+            //TODO: track
         }
     }
 
