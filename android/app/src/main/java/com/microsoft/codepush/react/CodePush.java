26c213deff81fd1c56b0abbcbf4591d5c5d05e7a
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-06-22 10:36:19 +0300

    Check for null

--- android/app/src/main/java/com/microsoft/codepush/react/CodePush.java
+++ android/app/src/main/java/com/microsoft/codepush/react/CodePush.java
@@ -160,6 +160,9 @@ public class CodePush implements ReactPackage {
 
     public String getPackageFolder() {
         JSONObject codePushLocalPackage = mUpdateManager.getCurrentPackage();
+        if (codePushLocalPackage == null) {
+            return null;
+        }
         return mUpdateManager.getPackageFolderPath(codePushLocalPackage.optString("packageHash"));
     }
 
