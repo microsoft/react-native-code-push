91d5ed843fbcc8cfdce1a6f44add85e7d0efa49f
Nine To Four <daodidem@gmail.com>
2019-10-14 16:47:21 +0700

    postlink and postunlink

--- react-native.config.js
+++ react-native.config.js
@@ -7,6 +7,10 @@ module.exports = {
     },
     android: {
       packageInstance: "new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG)"
-    },
+    }
+  },
+  hooks: {
+    "postlink": "node node_modules/react-native-code-push/scripts/postlink/run",
+    "postunlink": "node node_modules/react-native-code-push/scripts/postunlink/run"
   }
 };
