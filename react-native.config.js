3c42f4cf118fd506832e1d94cdab82090d3f758a
Nine To Four <daodidem@gmail.com>
2019-10-22 16:22:05 +0700

    Alphabetical order

--- react-native.config.js
+++ react-native.config.js
@@ -1,16 +1,16 @@
 module.exports = {
+  hooks: {
+    "postlink": "node node_modules/react-native-code-push/scripts/postlink/run",
+    "postunlink": "node node_modules/react-native-code-push/scripts/postunlink/run"
+  },
   project: {
+    android: {
+      packageInstance: "new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG)"
+    },
     ios: {
       sharedLibraries: [
         "libz"
       ]
-    },
-    android: {
-      packageInstance: "new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG)"
     }
-  },
-  hooks: {
-    "postlink": "node node_modules/react-native-code-push/scripts/postlink/run",
-    "postunlink": "node node_modules/react-native-code-push/scripts/postunlink/run"
   }
 };
