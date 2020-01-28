1781e3a4be99d4e1b15cd148903f4b3f1a8b5544
Andrei Dubov <andrei.dubov@akvelon.com>
2020-01-28 11:59:51 +0300

    Add state in index.js

--- test/template/index.js
+++ test/template/index.js
@@ -24,6 +24,7 @@ class CODE_PUSH_TEST_APP_NAME extends Component {
     // CodePush API Callbacks
     constructor() {
         super();
+        this.state = { message: '' };
     }
     // checkForUpdate
     checkUpdateSuccess(remotePackage) {
