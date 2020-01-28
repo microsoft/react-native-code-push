a90f68fca2c4a8b64ff89bd56075a64efe8e5eb8
Alexander Goncharov <v-algonc@microsoft.com>
2020-01-28 16:58:05 +0300

    Update AppDelegate.m in test template

--- test/template/ios/TestCodePush/AppDelegate.m
+++ test/template/ios/TestCodePush/AppDelegate.m
@@ -1,28 +1,28 @@
 #import "AppDelegate.h"
 
-#import "RCTRootView.h"
+#import <CodePush/CodePush.h>
 
-#import "CodePush.h"
-
-#import "RCTLog.h"
+#import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  RCTSetLogThreshold(RCTLogLevelInfo);
-
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [CodePush bundleURL];
 
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"CODE_PUSH_TEST_APP_NAME"
-                                               initialProperties:nil
-                                                   launchOptions:launchOptions];
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:jsCodeLocation
+                                                   moduleName:@"CODE_PUSH_TEST_APP_NAME"
+                                            initialProperties:nil];
+
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [[UIViewController alloc] init];
+  UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
