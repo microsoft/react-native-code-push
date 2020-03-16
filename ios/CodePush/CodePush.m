fe5b0c1b86f200ea9ac0b303c2ab7e7e2fac70c1
Andrey Dubov <andrei.dubov@akvelon.com>
2020-03-16 08:22:42 +0300

    Add promise resolve (#1810)

--- ios/CodePush/CodePush.m
+++ ios/CodePush/CodePush.m
@@ -902,6 +902,7 @@ RCT_EXPORT_METHOD(setLatestRollbackInfo:(NSString *)packageHash
                   reject:(RCTPromiseRejectBlock)reject)
 {
     [[self class] setLatestRollbackInfo:packageHash];
+    resolve(nil);
 }
 
 
