515838d336b67228c7c5028f6a742bef7ce2986a
alexChernyatiev <v-calexa@microsoft.com>
2021-10-19 17:33:51 +0300

    Changed yml: moved setup to separate task

--- test/test.ts
+++ test/test.ts
@@ -198,10 +198,7 @@ class RNIOS extends Platform.IOS implements RNPlatform {
             .then(TestUtil.copyFile.bind(undefined,
                 path.join(TestConfig.templatePath, "ios", TestConfig.TestAppName, "AppDelegate.m"),
                 appDelegatePath, true))
-            .then<void>(TestUtil.replaceString.bind(undefined, appDelegatePath, TestUtil.CODE_PUSH_TEST_APP_NAME_PLACEHOLDER, TestConfig.TestAppName))
-            .then<void>(() => {setTimeout(function(){ // sleep 5s
-                // do what you need here
-            }, 5000); });
+            .then<void>(TestUtil.replaceString.bind(undefined, appDelegatePath, TestUtil.CODE_PUSH_TEST_APP_NAME_PLACEHOLDER, TestConfig.TestAppName));
     }
 
     /**
