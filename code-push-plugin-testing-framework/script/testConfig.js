1fd188c4a862b949587ff3dd6666949f6b9df535
Evgenii Utkin <evutkin@microsoft.com>
2023-01-03 10:40:44 +0100

    Update testConfig.js

--- code-push-plugin-testing-framework/script/testConfig.js
+++ code-push-plugin-testing-framework/script/testConfig.js
@@ -9,7 +9,7 @@ var TestUtil_1 = require("./TestUtil");
 // COMMAND LINE OPTION NAMES, FLAGS, AND DEFAULTS
 var DEFAULT_TEST_RUN_DIRECTORY = path.join(os.tmpdir(), TestUtil_1.TestUtil.getPluginName(), "test-run");
 var DEFAULT_UPDATES_DIRECTORY = path.join(os.tmpdir(), TestUtil_1.TestUtil.getPluginName(), "updates");
-var DEFAULT_PLUGIN_PATH = path.join(__dirname, "../../..");
+var DEFAULT_PLUGIN_PATH = path.join(__dirname, "../..");
 var NPM_PLUGIN_PATH = TestUtil_1.TestUtil.getPluginName();
 var SETUP_FLAG_NAME = "--setup";
 var DEFAULT_PLUGIN_TGZ_NAME = TestUtil_1.TestUtil.getPluginName() + "-" + TestUtil_1.TestUtil.getPluginVersion() + ".tgz";
@@ -17,7 +17,7 @@ var DEFAULT_PLUGIN_TGZ_NAME = TestUtil_1.TestUtil.getPluginName() + "-" + TestUt
 exports.TestAppName = "TestCodePush";
 exports.TestNamespace = "com.testcodepush";
 exports.AcquisitionSDKPluginName = "code-push";
-exports.templatePath = path.join(__dirname, "../../../test/template");
+exports.templatePath = path.join(__dirname, "../../test/template");
 exports.thisPluginInstallString = TestUtil_1.TestUtil.resolveBooleanVariables(process.env.NPM) ? `npm install ${NPM_PLUGIN_PATH}` : `npm pack ${DEFAULT_PLUGIN_PATH} && npm install ${DEFAULT_PLUGIN_TGZ_NAME} && npm link`;
 exports.testRunDirectory = process.env.RUN_DIR ? process.env.RUN_DIR: DEFAULT_TEST_RUN_DIRECTORY;
 exports.updatesDirectory = process.env.UPDATE_DIR ? process.env.UPDATE_DIR : DEFAULT_UPDATES_DIRECTORY;
