a50a91bf646d575d1e37414046887496374d5bc9
Davyd <4661784+retyui@users.noreply.github.com>
2023-02-04 09:18:43 +0100

    CodePushDialog.showDialog show incorrect buttons order on Android
    fix: #2420

--- AlertAdapter.js
+++ AlertAdapter.js
@@ -5,17 +5,20 @@ if (Platform.OS === "android") {
   const { NativeModules: { CodePushDialog } } = React;
     
   Alert = {
-    alert(title, message, buttons) {
+    alert(title, message, rawButtons) {
+      // Fix for #2420 (incorrect button order)
+      const buttons = Array.from(rawButtons).reverse();
+
       if (buttons.length > 2) {
         throw "Can only show 2 buttons for Android dialog.";
       }
-      
+
       const button1Text = buttons[0] ? buttons[0].text : null,
             button2Text = buttons[1] ? buttons[1].text : null;
-      
+
       CodePushDialog.showDialog(
         title, message, button1Text, button2Text,
-        (buttonId) => { buttons[buttonId].onPress && buttons[buttonId].onPress(); }, 
+        (buttonId) => { buttons[buttonId].onPress && buttons[buttonId].onPress(); },
         (error) => { throw error; });
     }
   };
