9ac6ea9b8f50e56192be1d8574daf9078f941f49
David <david@latitude.io>
2023-08-11 20:52:11 -0400

    Update buildscriptDependency.ts
    Remove deprecated comments.

--- plugin/src/ios/buildscriptDependency.ts
+++ plugin/src/ios/buildscriptDependency.ts
@@ -2,10 +2,6 @@ import { ConfigPlugin, withInfoPlist } from 'expo/config-plugins'
 
 import { PluginConfigType } from '../pluginConfig'
 
-// Use these imports in SDK 46 and lower
-// import { ConfigPlugin, InfoPlist, withInfoPlist } from '@expo/config-plugins';
-// import { ExpoConfig } from '@expo/config-types';
-
 // Pass `<string>` to specify that this plugin requires a string property.
 export const withIosBuildscriptDependency: ConfigPlugin<PluginConfigType> = (config, props) => {
   if (!props.ios.CodePushDeploymentKey) return config
