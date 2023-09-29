f9208a7c285f5db152641048ca13f4d9c2ea3510
deggertsen <david@latitude.io>
2023-09-29 01:12:46 -0400

    Fix expo config plugins imports

--- plugin/src/ios/buildscriptDependency.ts
+++ plugin/src/ios/buildscriptDependency.ts
@@ -1,4 +1,4 @@
-import { ConfigPlugin, withInfoPlist } from 'expo/config-plugins'
+import { ConfigPlugin, withInfoPlist } from '@expo/config-plugins'
 
 import { PluginConfigType } from '../pluginConfig'
 
