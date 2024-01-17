99c8c4e4e01d27353c2db52b1c274cf284685531
Dima <v-dmkira@microsoft.com>
2024-01-17 22:44:54 +0700

    Replace DevInternalSettings with DeveloperSettings for react-native 0.73.0 (#2617)
    * Replace DevInternalSettings with DeveloperSettings
    
    DevInternalSettings was enclosed in ReactNative 0.73.0-rc.2, so we need to replace it with public class DeveloperSettings.
    https://github.com/facebook/react-native/pull/37256
    
    * auto install cocoapods in tests
    
    ---------
    
    Co-authored-by: velimir-jankovic <84719115+velimir-jankovic@users.noreply.github.com>
    Co-authored-by: JiglioNero <mikhail.suendukov@akvelon.com>

--- test/test.ts
+++ test/test.ts
@@ -307,7 +307,7 @@ class RNProjectManager extends ProjectManager {
         }
         mkdirp.sync(projectDirectory);
 
-        return TestUtil.getProcessOutput("npx react-native init " + appName + " --version 0.71.3", { cwd: projectDirectory, timeout: 30 * 60 * 1000 })
+        return TestUtil.getProcessOutput("npx react-native init " + appName + " --version 0.71.3 --install-pods", { cwd: projectDirectory, timeout: 30 * 60 * 1000 })
             .then((e) => { console.log(`"npx react-native init ${appName}" success. cwd=${projectDirectory}`); return e; })
             .then(this.copyTemplate.bind(this, templatePath, projectDirectory))
             .then<void>(TestUtil.getProcessOutput.bind(undefined, TestConfig.thisPluginInstallString, { cwd: path.join(projectDirectory, TestConfig.TestAppName) }))
