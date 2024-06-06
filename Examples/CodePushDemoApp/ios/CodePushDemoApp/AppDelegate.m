7978a7fe586a633b3c6c158bf0608f8826568dd7
Juliovaz <julio.oliveiravaz0@gmail.com>
2024-06-06 00:31:32 -0300

    refactor: remove unnecessary fallbackExtension from demoapp

--- Examples/CodePushDemoApp/ios/CodePushDemoApp/AppDelegate.m
+++ Examples/CodePushDemoApp/ios/CodePushDemoApp/AppDelegate.m
@@ -54,7 +54,7 @@ static void InitializeFlipper(UIApplication *application) {
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackExtension:nil];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
   return [CodePush bundleURL];
 #endif
