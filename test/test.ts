755068a4e4a3b86ee12a36b6f5002972ed7e3572
Minsik Kim <kmsbernard@gmail.com>
2025-01-04 23:44:12 +0900

    use rn0.76.5 in test

--- test/test.ts
+++ test/test.ts
@@ -315,7 +315,7 @@ class RNProjectManager extends ProjectManager {
         }
         mkdirp.sync(projectDirectory);
 
-        return TestUtil.getProcessOutput("npx @react-native-community/cli init " + appName + " --version 0.76.1 --install-pods", { cwd: projectDirectory, timeout: 30 * 60 * 1000 })
+        return TestUtil.getProcessOutput("npx @react-native-community/cli init " + appName + " --version 0.76.5 --install-pods", { cwd: projectDirectory, timeout: 30 * 60 * 1000 })
             .then((e) => { console.log(`"npx @react-native-community/cli init ${appName}" success. cwd=${projectDirectory}`); return e; })
             .then(this.copyTemplate.bind(this, templatePath, projectDirectory))
             .then<void>(TestUtil.getProcessOutput.bind(undefined, TestConfig.thisPluginInstallString, { cwd: path.join(projectDirectory, TestConfig.TestAppName) }))
